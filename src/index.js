f531fb0f97688316272e564eea1b412526c4b9aa
Abdiel Baldonedo <abdielr507@gmail.com>
2024-10-15 11:26:16 -0500

    refactor: separated dark mode logic to its own file
    in order to add dark mode toggle easily to other html pages

--- src/index.js
+++ src/index.js
@@ -195,28 +195,3 @@ document.getElementById("moreInfoButton").addEventListener("click", function() {
 // Initialize the gallery with the first project
 updateGallery();
 
-// Better Dark mode toggle
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.body.classList.add('dark-mode');
-    localStorage.setItem('theme', 'dark');
-  } else {
-    document.body.classList.remove('dark-mode');
-    localStorage.setItem('theme', 'light');
-  }    
-}
-
-toggleSwitch.addEventListener('change', switchTheme, false);
-
-// Check for saved user preference, if any, on load of the website
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme) {
-  document.body.classList[currentTheme === 'dark' ? 'add' : 'remove']('dark-mode');
-
-  if (currentTheme === 'dark') {
-    toggleSwitch.checked = true;
-  }
-}
